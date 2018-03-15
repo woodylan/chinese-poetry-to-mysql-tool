be89ffc176e13dd30a502ff37e94f6f6999c6955
woodylan <woodylan@126.com>
2018-03-15 18:08:12 +0800

    Determine if chinese-poetry  repertory exists

--- work.php
+++ work.php
@@ -4,6 +4,12 @@ $dirPath = dirname(__FILE__);
 
 $sourceFilePath = $dirPath . 'chinese-poetry/json/';
 
+//判断古诗词仓库是否存在
+$isPathExist = file_exists($sourceFilePath);
+if ($isPathExist == false) {
+    gitClone($dirPath);
+}
+
 $sqlFileName = 'chinese-poetry.sql';
 
 $sqlPath = $dirPath . $sqlFileName;
@@ -43,3 +49,43 @@ foreach ($tangFilePathList as $filePath) {
 //最后一行添加分号
 $handle = fopen($sqlPath, 'a+');
 fwrite($handle, ';');
+
+
+function gitClone($path)
+{
+    echo "检查git是否存在\n";
+
+    list($status, $log) = runLocalCommand('git --version');
+    if ($status == false) {
+        echo "git 不存在，请安装\n";
+        die($log);
+    }
+
+    $cmd = [
+        sprintf('cd %s', $path),
+        'git clone https://github.com/chinese-poetry/chinese-poetry.git'
+
+    ];
+
+    echo "下载古诗词仓库\n";
+    $command = join(' && ', $cmd);
+    list($status, $log) = runLocalCommand($command);
+
+    if ($status == false) {
+        echo "git 不存在，请安装\n";
+        die($log);
+    }
+}
+
+function runLocalCommand($command)
+{
+    exec(trim($command) . ' 2>&1', $log, $statusNum);
+    $log = implode($log, "\r\n");
+
+    $status = true;
+    if ($statusNum != 0) {
+        $status = false;
+    }
+
+    return [$status, $log];
+}
