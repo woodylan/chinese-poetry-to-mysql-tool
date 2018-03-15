766498899c4dab9dd8bba73645639ff253f93331
woodylan <woodylan@126.com>
2018-03-15 18:10:19 +0800

    cancel determine git

--- work.php
+++ work.php
@@ -7,7 +7,7 @@ $sourceFilePath = $dirPath . 'chinese-poetry/json/';
 //判断古诗词仓库是否存在
 $isPathExist = file_exists($sourceFilePath);
 if ($isPathExist == false) {
-    gitClone($dirPath);
+    die('古诗词仓库不存在，请按说明下载');
 }
 
 $sqlFileName = 'chinese-poetry.sql';
@@ -49,43 +49,3 @@ foreach ($tangFilePathList as $filePath) {
 //最后一行添加分号
 $handle = fopen($sqlPath, 'a+');
 fwrite($handle, ';');
-
-
-function gitClone($path)
-{
-    echo "检查git是否存在\n";
-
-    list($status, $log) = runLocalCommand('git --version');
-    if ($status == false) {
-        echo "git 不存在，请安装\n";
-        die($log);
-    }
-
-    $cmd = [
-        sprintf('cd %s', $path),
-        'git clone https://github.com/chinese-poetry/chinese-poetry.git'
-
-    ];
-
-    echo "下载古诗词仓库\n";
-    $command = join(' && ', $cmd);
-    list($status, $log) = runLocalCommand($command);
-
-    if ($status == false) {
-        echo "git 不存在，请安装\n";
-        die($log);
-    }
-}
-
-function runLocalCommand($command)
-{
-    exec(trim($command) . ' 2>&1', $log, $statusNum);
-    $log = implode($log, "\r\n");
-
-    $status = true;
-    if ($statusNum != 0) {
-        $status = false;
-    }
-
-    return [$status, $log];
-}
